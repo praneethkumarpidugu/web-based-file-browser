20b4fae81317944f1eceb479a43aa48968cc90e5
praneethkumarpidugu <pidugu2007@gmail.com>
2017-05-10 22:07:25 -0400

    Able to Print out the Files by its size and path name
    Todo :- Maintain a Reverse Listing of all items in the string

--- back-end/app.js
+++ back-end/app.js
@@ -5,13 +5,18 @@ const fs = require('fs');
 
 var Client = require('ssh2').Client;
 
+const http = require('http');
+
 var conn = new Client();
 
-var my = conn.on('ready', function() {
+// hmy result has all the streamed data from the remote linux machine.
+
+var myresult = [];
+
+var sshConnection = conn.on('ready', function() {
         console.log('Client :: ready');
         shellData = conn.shell(function(err, stream) {
 
-            var myresult = [];
             if (err) throw err;
             var result = stream.on('close', function() {
                 console.log('Stream :: close');
@@ -19,25 +24,55 @@ var my = conn.on('ready', function() {
             }).on('data', function(data) {
                 // console.log('STDOUT: ' + data);
                 myresult.push(data);
-                fs.writeFileSync('data2.txt', myresult);
+                fs.writeFileSync('data.txt', myresult);
+
             }).stderr.on('data', function(data) {
                 console.log('STDERR: ' + data);
             });
-            console.log("My Result"+result);
-            // stream.end('ls -l\nexit\n');
 
-            // console.log("MY RESULT: " + result);
            stream.end("du -h --all\nexit\n");
-           // fs.writeFileSync('data.txt', shellData);
+
 
         });
     }).connect({
-        host: 'YOUR HOST IP ADDRESS',
-        port: '22',
+        host: 'YOUR IP ADDRESS',
+        port: 'YOUR PORT NUMBER',
         username: 'YOUR USERNAME',
         password: 'YOUR PASSWORD'
 
     });
-debugger;
-// fs.writeFileSync('data.txt', my);
-// console.log("###############", typeof conn);
+
+
+var myData = fs.readFileSync('data.txt', 'utf8');
+
+var lines = myData.split('\n');
+
+//Have Properties for file size, file name according
+//to their id's
+
+var ourData = [];
+
+
+var obj = {
+    fileSize: ourData[0] ,
+    Path: ourData[1]
+};
+
+
+
+var newLine = lines.splice(15);
+// console.log(myData.split('\n'));
+for (var i=0; i<newLine.length; i++) {
+    // console.log(newLine[i].replace(/\,/g,""));
+    var replacedData = newLine[i].replace(/\,/g,"");
+    ourData.push(replacedData.split('\t'));
+}
+//Our Final Data with file size and Path Loged.
+
+
+for (var j=0; j < ourData.length; j++) {
+
+    console.log("ID: " + j + " File Size: " + ourData[j][0] + " File Path: " + ourData[j][1]);
+}
+
+// console.log(ourData.reverse());
