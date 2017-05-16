b44dcc301711faa0e7090e8e9fdea3ea70d96082
praneethkumarpidugu <pidugu2007@gmail.com>
2017-05-16 13:41:37 -0400

    [Satisfies partial requirement]

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # web-based-file-browser
+
+
+# Getting Started
+
+## Back-end
+
+- Subsitute Ip address, Username, Password
+
+#### $ node app.js
+
+-> Generates JSON file with data of File Path
+and File Size
+
+## Front-end
+
+#### Open FileBrowser.html with files recursively
+#### Listed with Size and Path.
--- front-end/app/main.js
+++ front-end/app/main.js
@@ -1,9 +1,20 @@
 var app = angular.module('webfile', []);
 
-app.controller('LoginController', function ($scope) {
+app.controller('LoginController', function ($scope, $http) {
 	$scope.username = "";
+	$scope.ipaddress = "";
+	$scope.password = "";
+
+
 	console.log($scope);
 });
 
+app.controller('FileBrowserController', function ($scope, $http) {
+	$scope.mydata = "";
+	$http.get('../../back-end/myJSONData.json')
+		.then(function (res) {
+			$scope.mydata = res.data;
+        });
+});
 
 
